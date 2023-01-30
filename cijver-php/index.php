86040f66237262a91cf2fbd7458595e84e73832a
Dylan <dylchelvia@gmail.com>
2023-01-30 16:50:47 -0300

    k

---
+++ cijver-php/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link rel="stylsheet" href="style.css">
+    <style>
+        * {
+            padding: 0;
+            margin: 0;
+        }
+
+        ul {
+            list-style-type: none;
+        }
+
+        li {
+            display: inline;
+            margin: auto;
+        }
+
+        nav {
+            height: 50px;
+        }
+    </style>
+</head>
+
+<body>
+    <nav>
+        <ul>
+            <li><a>Home</a></li>
+            <li><a>Store</a></li>
+            <li><a>About</a></li>
+            <li><a>Contact</a></li>
+        </ul>
+    </nav>
+    <div>
+        <h1></h1>
+    </div>
+</body>
+
+</html>
