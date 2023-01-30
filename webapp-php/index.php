1ebe48b20287aa3c91f173975c669c363c4b5005
Dylan <dylchelvia@gmail.com>
2023-01-30 09:28:16 -0300

    v1

--- webapp-php/index.php
+++ webapp-php/index.php
@@ -1,9 +1,19 @@
 <!DOCTYPE html>
 <html>
+    <head>
+        <link rel="stylsheet" href="style.css">
+    </head>
     <body>
-        <?php
-        $i = 0;
-        echo $i;
-        ?>
+        <nav>
+            <ul>
+                <li><a>Home</a></li>
+                <li><a>Store</a></li>
+                <li><a>About</a></li>
+                <li><a>Contact</a></li>
+            </ul>
+        </nav>
+        <div>
+            <h1></h1>
+        </div>
     </body>
 </html>
